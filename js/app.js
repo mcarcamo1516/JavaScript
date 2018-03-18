be4292e3fd0a76b973eeb40fb6f312d61d77da24
mcarcamo1516 <35343238+mcarcamo1516@users.noreply.github.com>
2018-03-18 13:13:58 -0300

    click botones

---
+++ js/app.js
@@ -0,0 +1,110 @@
+var on = document.getElementById('on');
+var sign = document.getElementById('sign');
+var raiz = document.getElementById('raiz');
+var uno = document.getElementById('1');
+var dos = document.getElementById('2');
+var tres = document.getElementById('3');
+var cuatro = document.getElementById('4');
+var cinco = document.getElementById('5');
+var seis = document.getElementById('6');
+var siete = document.getElementById('7');
+var ocho = document.getElementById('8');
+var nueve = document.getElementById('9');
+var zero = document.getElementById('0');
+var dividido = document.getElementById('dividido');
+var por = document.getElementById('por');
+var menos = document.getElementById('menos');
+var mas = document.getElementById('mas');
+var punto = document.getElementById('punto');
+var igual = document.getElementById('igual');
+
+
+on.onclick = function() {
+    on.style.height = "60px";
+    on.style.width = "21%";
+
+}
+sign.onclick = function() {
+    sign.style.height = "60px";
+    sign.style.width = "21%";
+
+}
+raiz.onclick = function() {
+    raiz.style.height = "60px";
+    raiz.style.width = "21%";
+
+}
+dividido.onclick = function() {
+    dividido.style.height = "60px";
+    dividido.style.width = "21%";
+
+}
+por.onclick = function() {
+    por.style.height = "60px";
+    por.style.width = "21%";
+
+}
+menos.onclick = function() {
+    menos.style.height = "60px";
+    menos.style.width = "21%";
+
+}
+mas.onclick = function() {
+    mas.style.height = "130px";
+    
+
+}
+punto.onclick = function() {
+    punto.style.height = "60px";
+    punto.style.width = "28%";
+
+}
+igual.onclick = function() {
+    igual.style.height = "60px";
+    igual.style.width = "28%";
+
+}
+uno.onclick = function() {
+    uno.style.height = "60px";
+    uno.style.width = "28%";
+}
+dos.onclick = function() {
+    dos.style.height = "60px";
+   dos.style.width = "28%";
+}
+tres.onclick = function() {
+    tres.style.height = "60px";
+    tres.style.width = "28%";
+
+}
+cuatro.onclick = function() {
+    cuatro.style.height = "60px";
+    cuatro.style.width = "21%";
+}
+cinco.onclick = function() {
+    cinco.style.height = "60px";
+    cinco.style.width = "21%";  
+}
+seis.onclick = function() {
+    seis.style.height = "60px";
+    seis.style.width = "21%";
+}
+siete.onclick = function() {
+    siete.style.height = "60px";
+    siete.style.width = "21%";
+
+}
+ocho.onclick = function() {nueve
+    ocho.style.height = "60px";
+    ocho.style.width = "21%";
+
+}
+nueve.onclick = function() {
+    nueve.style.height = "60px";
+    nueve.style.width = "21%";
+
+}
+zero.onclick = function() {
+    zero.style.height = "60px";
+    zero.style.width = "28%";
+}
